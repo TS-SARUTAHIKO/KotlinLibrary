6be4a7fc2a1374f63ced64403ba5b0f21224784a
tshom_000 <tshomeaddress@gmail.com>
2021-02-15 13:52:46 +0900

    1.1.20

--- src/main/kotlin/com/xxxsarutahikoxxx/kotlin/Utilitys/utils.kt
+++ src/main/kotlin/com/xxxsarutahikoxxx/kotlin/Utilitys/utils.kt
@@ -1,5 +1,6 @@
 package com.xxxsarutahikoxxx.kotlin.Utilitys
 
+import java.io.File
 import java.io.InputStream
 import java.lang.RuntimeException
 
@@ -14,6 +15,17 @@ var out : Any?
     get() = throw RuntimeException("標準出力ストリームは書き込み専用です")
     set(value) { outstream(value) }
 
+var File.out : String
+    get(){
+        return inputStream().use {
+            it.bufferedReader().use { it.readText() }
+        }
+    }
+    set(value) {
+        outputStream().use {
+            it.bufferedWriter().use { it.write(value) }
+        }
+    }
 
 /** Pair<A, B> から Triple<A, B, C> を作る関数 */
 infix fun <A, B, C> Pair<A, B>.tt( third : C ) : Triple<A, B, C> = Triple(first, second, third)
